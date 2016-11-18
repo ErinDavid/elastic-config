b8be0a9731a03c672b004b71c46fdf0237e006ef
zhangwei8 <zhangweihlwrj@bbktel.com>
2016-11-18 18:34:11 +0800

    增加maven中央仓库相关pom依赖，发布snapshot版本到maven中央仓库
    Change-Id: I92f356dd7e7f6716ccb2f5fdbc8e815c4cc9b9e0

--- elastic-config-core/src/main/java/com/github/config/listener/zookeeper/ZookeeperListenerManager.java
+++ elastic-config-core/src/main/java/com/github/config/listener/zookeeper/ZookeeperListenerManager.java
@@ -30,76 +30,126 @@ import com.github.config.listener.ElaticCofnigEventListener;
 @RequiredArgsConstructor
 public class ZookeeperListenerManager extends AbstractListenerManager {
 
-    private final ZookeeperElasticConfigGroup zookeeperConfigGroup;
+	private final ZookeeperElasticConfigGroup zookeeperConfigGroup;
 
-    @Override
-    public void start() {
-        addDataListener(new ConfigChangedConfigListener());
-        addConnectionStateListener(new ConnectionLostListener());
-        addEventListenerStateListener(new ElaticCofnigEventListener(zookeeperConfigGroup));
-    }
+	@Override
+	public void start() {
+		addDataListener(new ConfigChangedConfigListener());
+		addConnectionStateListener(new ConnectionLostListener());
+		addEventListenerStateListener(new ElaticCofnigEventListener(
+				zookeeperConfigGroup));
+	}
 
-    class ConfigChangedConfigListener extends AbstractConfigListener {
+	class ConfigChangedConfigListener extends AbstractConfigListener {
 
-        @Override
-        protected void dataChanged(final CuratorFramework client, final TreeCacheEvent event, final String path) {
+		@Override
+		protected void dataChanged(final CuratorFramework client,
+				final TreeCacheEvent event, final String path) {
 
-            if (!isStopped(client) && isNotified(client, event, path)) {
-                reload(path);
-                pushEvent(event, path);
-            }
-        }
-    }
+			if (!isStopped(client) && isNotified(client, event, path)) {
+				String oldvalue = oldvalue(path);
+				ZookeeperListenerManager.this.reload(path);
+				ZookeeperListenerManager.this.pushEvent(event, path, oldvalue);
+			}
+		}
+	}
 
-    class ConnectionLostListener implements ConnectionStateListener {
+	class ConnectionLostListener implements ConnectionStateListener {
 
-        @Override
-        public void stateChanged(final CuratorFramework client, final ConnectionState newState) {
-            log.info("zookeeper connection state changed.new state:{}", newState);
-            if (ConnectionState.RECONNECTED == newState) {
-                zookeeperConfigGroup.loadNode();
-            }
-        }
-    }
+		@Override
+		public void stateChanged(final CuratorFramework client,
+				final ConnectionState newState) {
+			log.info("zookeeper connection state changed.new state:{}",
+					newState);
+			if (ConnectionState.RECONNECTED == newState) {
+				zookeeperConfigGroup.loadNode();
+			}
+		}
+	}
 
-    @Override
-    protected void addDataListener(TreeCacheListener listener) {
-        zookeeperConfigGroup.getConfigNodeStorage().addDataListener(listener);
-    }
+	@Override
+	protected void addDataListener(TreeCacheListener listener) {
+		zookeeperConfigGroup.getConfigNodeStorage().addDataListener(listener);
+	}
 
-    @Override
-    protected void addConnectionStateListener(ConnectionStateListener listener) {
-        zookeeperConfigGroup.getConfigNodeStorage().addConnectionStateListener(listener);
-    }
+	@Override
+	protected void addConnectionStateListener(ConnectionStateListener listener) {
+		zookeeperConfigGroup.getConfigNodeStorage().addConnectionStateListener(
+				listener);
+	}
 
-    @Override
-    protected void addEventListenerStateListener(EventListener listener) {
-        listener.register();
-    }
+	@Override
+	protected void addEventListenerStateListener(EventListener listener) {
+		listener.register();
+	}
 
-    private void reload(String path) {
-        log.debug("reload the config node:{}", ZKPaths.getNodeFromPath(path));
-        String key = ZKPaths.getNodeFromPath(path);
-        zookeeperConfigGroup.reloadKey(key);
-    }
+	private String oldvalue(final String path) {
+		return zookeeperConfigGroup.get(ZKPaths.getNodeFromPath(path));
+	}
 
-    private void pushEvent(final TreeCacheEvent event, String path) {
-        String key = ZKPaths.getNodeFromPath(path);
-        String value = zookeeperConfigGroup.getConfigNodeStorage().getConfigNodeDataDirectly(key);
-        if (event.getType() == Type.NODE_UPDATED && !StringUtils.equals(value, zookeeperConfigGroup.get(key))) {
-            ElasticConfigEventBus.pushEvent(ElasticConfigEvent.builder().path(path).value(value)
-                .eventType(eventMap.get(event.getType())).build());
-        }
-    }
+	/**
+	 * 重加载路径path配置节点
+	 * 
+	 * @param path
+	 *            配置节点路径
+	 */
+	private void reload(String path) {
+		log.debug("reload the config node:{}", ZKPaths.getNodeFromPath(path));
+		String key = ZKPaths.getNodeFromPath(path);
+		zookeeperConfigGroup.reloadKey(key);
+	}
 
-    private boolean isStopped(final CuratorFramework client) {
-        return client.getState() == CuratorFrameworkState.STOPPED;
-    }
+	/**
+	 * 发布事件
+	 * 
+	 * @param event
+	 *            配置节点变更事件
+	 * @param path
+	 *            配置节点路径
+	 * @param oldvalue
+	 *            配置节点原始值
+	 */
+	private void pushEvent(final TreeCacheEvent event, String path,
+			String oldvalue) {
+		String key = ZKPaths.getNodeFromPath(path);
+		String value = zookeeperConfigGroup.getConfigNodeStorage()
+				.getConfigNodeDataDirectly(key);
+		if (event.getType() == Type.NODE_UPDATED
+				&& !StringUtils.equals(value, oldvalue)) {
+			ElasticConfigEventBus.pushEvent(ElasticConfigEvent.builder()
+					.path(path).value(value)
+					.eventType(eventMap.get(event.getType())).build());
+		}
+	}
 
-    private boolean isNotified(final CuratorFramework client, final TreeCacheEvent event, final String path) {
-        log.info("elastic config node change.type:{},path:{}", event.getType(), path);
-        return zookeeperConfigGroup.getConfigNodeStorage().getConfigProfile()
-            .getFullPath(ZKPaths.getNodeFromPath(path)).equals(path)
-            && (eventMap.containsKey(event.getType()));
-    }
+	/**
+	 * 判断Zookeeper客户端是是否已径关闭
+	 * 
+	 * @param client
+	 *            Zookeeper客户端
+	 * @return 是否已径关闭
+	 */
+	private boolean isStopped(final CuratorFramework client) {
+		return client.getState() == CuratorFrameworkState.STOPPED;
+	}
+
+	/**
+	 * 判断配置节点变更时是否需要发送消息知通
+	 * 
+	 * @param client
+	 *            zookeeper客户端
+	 * @param event
+	 *            配置节点变更事件
+	 * @param path
+	 *            配置节点路径
+	 * @return 是否通知
+	 */
+	private boolean isNotified(final CuratorFramework client,
+			final TreeCacheEvent event, final String path) {
+		log.info("elastic config node change.type:{},path:{}", event.getType(),
+				path);
+		return zookeeperConfigGroup.getConfigNodeStorage().getConfigProfile()
+				.getFullPath(ZKPaths.getNodeFromPath(path)).equals(path)
+				&& (eventMap.containsKey(event.getType()));
+	}
 }
